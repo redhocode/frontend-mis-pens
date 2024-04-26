1ec2f11be8566d7d1d4f60215ad17c18597a495e
Redho Arifin <redhoarifin2017@gmail.com>
2024-04-26 19:14:05 +0900

    back local

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://192.168.0.104:9000",
+    baseURL: "http://localhost:9000",
       withCredentials: true,
 
 })
