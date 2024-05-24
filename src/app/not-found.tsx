b1e4a0b9f8ef275e6c61655f33f974fe9d822f1e
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-24 15:43:51 +0900

    add css

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,19 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <h1 className='text-9xl animate-bounce'>404</h1>
+      <h2 className='mb-4 text-xl'>Page not found</h2>
+      <Link href={'/'}>
+        <Button variant="outline" size="sm">Go Home</Button>
+        </Link>
+      
     </div>
   )
 }
