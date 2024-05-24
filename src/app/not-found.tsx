bbec8815914fb3b7784b45144966ea5024e1a35b
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-24 15:22:08 +0900

    fix page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export const notFound = () => {
+
+
+export default function NotFound() {
   return (
-    <div>not-found</div>
+    <div>
+      <h1>Not Found</h1>
+    </div>
   )
 }
