6955fa96f8e000dcb2809c920c7ffe0374c117a8
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-26 01:12:35 +0900

    add authguard

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -9,6 +9,7 @@ import {
 import { Metadata } from "next";
 import Script from "next/script";
 import { PrimeReactProvider } from "primereact/api";
+import AuthGuard from "@/components/AuthGuard";
 export const metadata: Metadata = {
   metadataBase: new URL("https://pjj.pens.ac.id"),
   icons: {
@@ -21,21 +22,21 @@ export const metadata: Metadata = {
 };
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-  
-        <section>
-          <div className="ml-[270px] mx-5 md:ml-4 bg-white dark:bg-zinc-800 py-4 min-h-screen">
-            {/* <Navbar /> */}
-            <Sidebar />
-            <Providers>
-              <PrimeReactProvider value={{unstyled: true}}>
+    <section>
+      <AuthGuard>
+        <div className="ml-[270px] mx-5 md:ml-4 bg-white dark:bg-zinc-800 py-4 min-h-screen">
+          {/* <Navbar /> */}
+          <Sidebar />
+          <Providers>
+            <PrimeReactProvider value={{ unstyled: true }}>
               <div className="flex flex-col flex-1 mx-4 my-4 md:mx-8 md:pl-2">
                 {children}
               </div>
-              </PrimeReactProvider>
-            </Providers>
-          </div>
-        </section>
-     
+            </PrimeReactProvider>
+          </Providers>
+        </div>
+      </AuthGuard>
+    </section>
   );
 }
 
