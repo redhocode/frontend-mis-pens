417d8af4fc663e1c84e5435e14632022831970fd
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-25 16:45:15 +0900

    add fitur show password

--- src/components/form/loginform.tsx
+++ src/components/form/loginform.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
@@ -14,6 +14,11 @@ import { JwtPayload } from "jsonwebtoken";
 const LoginForm = () => {
   const router = useRouter();
   const { toast } = useToast();
+   const [showPassword, setShowPassword] = useState(false);
+
+   const toggleShowPassword = () => {
+     setShowPassword(!showPassword);
+   };
 const handleLogin = async (values: any, { setSubmitting, setErrors }: any) => {
   try {
     const response = await loginUser(values);
@@ -72,21 +77,19 @@ const handleLogin = async (values: any, { setSubmitting, setErrors }: any) => {
     <section>
       <div className="flex items-center justify-center">
         <div className=" max-w-max max-h-screen">
-         
-         
           <div className="flex flex-wrap shadow-lg px-20 py-20 justify-center ">
             <div className="outline outline-2 outline-secondary px-4 py-4 rounded-sm ">
-               <h1 className="mb-2 text-3xl font-bold text-center text-primary">
-            Welcome
-          </h1>
-               <p className="mb-8 font-medium text-center text-slate-800 ">
-            Login dulu cuy
-          </p>
+              <h1 className="mb-2 text-3xl font-bold text-center text-primary">
+                Welcome
+              </h1>
+              <p className="mb-8 font-medium text-center text-slate-800 ">
+                Login dulu cuy
+              </p>
               <Formik
-                initialValues={{ username: '', password: '' }}
+                initialValues={{ username: "", password: "" }}
                 validationSchema={Yup.object().shape({
-                  username: Yup.string().required('Username not empty'),
-                  password: Yup.string().required('Password not empty'),
+                  username: Yup.string().required("Username not empty"),
+                  password: Yup.string().required("Password not empty"),
                 })}
                 onSubmit={handleLogin}
               >
@@ -98,21 +101,43 @@ const handleLogin = async (values: any, { setSubmitting, setErrors }: any) => {
                         name="username"
                         placeholder="Username"
                         className="w-full mt-4 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-
                       />
-                      <ErrorMessage name="username" component="div" className="text-red-500" />
+                      <ErrorMessage
+                        name="username"
+                        component="div"
+                        className="text-red-500"
+                      />
                     </div>
                     <div>
                       <Field
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         name="password"
                         placeholder="Password"
                         className="w-full mt-4 py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                       />
-                      <ErrorMessage name="password" component="div" className="text-red-500" />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="text-red-500"
+                      />
                     </div>
-                    <Button type="submit" disabled={isSubmitting} className="w-full mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                      {isSubmitting ? 'Loading...' : 'Login'}
+                    <div className="mt-2">
+                      <input
+                        type="checkbox"
+                        id="showPassword"
+                        checked={showPassword}
+                        onChange={toggleShowPassword}
+                      />
+                      <label htmlFor="showPassword" className="ml-2">
+                        Show Password
+                      </label>
+                    </div>
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="w-full mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                    >
+                      {isSubmitting ? "Loading..." : "Login"}
                     </Button>
                   </Form>
                 )}
