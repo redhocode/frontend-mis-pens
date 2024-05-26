89042ace0cdb9650a12c1fd944176fcbb0fe2c76
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-26 12:48:53 +0900

    fix head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/navbar/navbar";
-import { SidebarDesktop } from "@/components/sidebar/sidebar-desktop";
-import Hero from "@/components/hero/hero";
-import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 import Providers from "@/components/providers";
-import Footer from "@/components/footer/footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
-import Head from "next/head";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 declare let dataLayer: any[];
 
@@ -21,7 +14,6 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://pjj.pens.ac.id"),
   icons: {
     icon: "/images/pens.png",
-
   },
   title: "D3 PJJ Teknik Informatika - Politeknik Elektro Negeri Surabaya",
   description:
@@ -35,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
@@ -50,16 +42,16 @@ export default function RootLayout({
         ></Script>
 
         <Script>
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-  
-  `}
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            
+            `}
         </Script>
-      </Head>
+      </head>
 
       <body className={`${inter.className}`} suppressHydrationWarning={true}>
         <main className="bg-slate-100 dark:bg-zinc-900 min-h-screen">
@@ -74,7 +66,7 @@ export default function RootLayout({
               {children}
               {process.env.NEXT_PUBLIC_GA_ID ? (
                 <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
-              ): null}
+              ) : null}
               <SpeedInsights />
             </Providers>
           </ThemeProvider>
