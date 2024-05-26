d5285e93e0ada529f0d534243b02f25b7dc4655e
Redho Arifin <redhoarifin2017@gmail.com>
2024-05-26 15:14:36 +0900

    add coutdown

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,7 +3,20 @@ import React, { useEffect, useState } from "react";
 import { useFetchUsers } from "@/features/useFetchData";
 import { User } from "@/types";
 import { useRouter } from "next/navigation";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+const renderTime = ({ remainingTime }: { remainingTime: number }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Too lale...</div>;
+  }
 
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text">Logout</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
 const Welcome = () => {
   // State untuk menyimpan informasi pengguna
  const [user, setUser] = useState<string | null>(null);
@@ -21,7 +34,7 @@ const Welcome = () => {
     <section>
       <div className="flex items-center justify-center">
         <div className=" w-full max-h-screen">
-          <div className="flex flex-wrap shadow-lg px-4 py-4 justify-center">
+          <div className="flex flex-wrap shadow-lg px-4 py-4 justify-between">
             <div className="outline outline-2 outline-secondary px-4 py-4 rounded-sm">
               <h1 className="mb-2 text-3xl font-bold text-center text-primary">
                 Welcome, {user}
@@ -32,6 +45,17 @@ const Welcome = () => {
               </p>
               {/* Your dashboard content here */}
             </div>
+            {/* <div className="justify-end flex">
+              <CountdownCircleTimer
+                isPlaying
+                duration={1800}
+                colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+                colorsTime={[10, 6, 3, 0]}
+                onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+              >
+                {renderTime}
+              </CountdownCircleTimer>
+            </div> */}
           </div>
         </div>
       </div>
